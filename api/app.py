8dd311028ab4e3403e9ba3d0162e1125bba67bbb
Daisy Kucharski <daisy.kucharski@gmail.com>
2022-06-30 19:17:16 -0400

    Set up docker and caching with redis

--- api/app.py
+++ api/app.py
@@ -1,28 +1,37 @@
 from flask import Flask, request
+from flask_caching import Cache
+
 import scrape
 
 app = Flask(__name__)
+app.config.from_object('config.BaseConfig')
+cache = Cache(app)
 
 @app.route('/birthday')
+@cache.cached(timeout=500, query_string=True)
 def get_birthday():
     name = request.args.get('name')
     return scrape.find_birthday(name)
 
 @app.route('/bestgifts')
+@cache.cached(timeout=500, query_string=True)
 def get_best_gifts():
     name = request.args.get('name')
     return scrape.find_best_gifts(name)
 
 @app.route('/universalloves')
+@cache.cached(timeout=500, query_string=True)
 def get_universal_loves():
     return scrape.find_universal_loves()
 
 @app.route('/fishinfo')
+@cache.cached(timeout=500, query_string=True)
 def get_fish_info():
     fish = request.args.get('fish')
     return scrape.find_fish_info(fish)
 
 @app.route('/cropinfo')
+@cache.cached(timeout=500, query_string=True)
 def get_crop_info():
     crop = request.args.get('crop')
     return scrape.find_crop_info(crop)
